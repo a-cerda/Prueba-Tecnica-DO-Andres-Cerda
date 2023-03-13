4c7419f54f01b4c10fcd1ad92519e6ac3a419224
Andrés Cerda Pimentel <12897630+a-cerda@users.noreply.github.com>
2023-03-13 00:02:32 -0300

    linting

--- Pregunta1/Patentes.py
+++ Pregunta1/Patentes.py
@@ -1,54 +1,62 @@
-# Primer problema de la prueba tecnica para Data Observatory
+""" Primer problema de la prueba tecnica para Data Observatory """
 
 # 1 Dada una patente retornar el id asociado (llave primaria)
 # 2 Dado un ID (llave primaria) Retornar la patente asociada.
 
 # Imports
-import string # para obtener digitos y letras
-import re # para formular la expresion regular
+import string  # para obtener digitos y letras
 
-#posibles_patentes = 
 # diccionario con la asociacion de letra a numero
-x = [letter for letter in string.ascii_uppercase] # Hacemos una lista con todas las letras
-letras_a_num: dict = dict(zip(x, map(x.index, x))) # Pasamos esa lista a un diccionario que las asocia a un numero
-num_a_letras = {v: k for k, v in letras_a_num.items()} # La lista inversa nos ayudara con la siguiente parte
-# Todas estas asociaciones se ocupan independientemente de cuantas veces llamemos a las funciones, por lo que vale
-# la pena mantenerlas en memoria precomputadas en vez de computarlas cada vez que llamemos a una funcion
+# Hacemos una lista con todas las letras
+x = [letter for letter in string.ascii_uppercase]
+# Pasamos esa lista a un diccionario que las asocia a un numero
+letras_a_num: dict = dict(zip(x, map(x.index, x)))
+# La lista inversa nos ayudara con la siguiente parte
+num_a_letras = {v: k for k, v in letras_a_num.items()}
+# Todas estas asociaciones se ocupan independientemente de cuantas veces llamemos
+# a las funciones, por lo que vale la pena mantenerlas en memoria precomputadas
+# en vez de computarlas cada vez que llamemos a una funcion
 
-# Pasa de una patente (en string) a un ID (en int)
-# Se asume una patente de la forma [A-Z]{4}[0-9]{3}
-def Patente_A_ID(patente: str) -> int:
+
+def patente_a_id(patente: str) -> int:
+    """ Pasa de una patente (en string) a un ID (en int)
+        Se asume una patente de la forma [A-Z]{4}[0-9]{3} """
     letras = patente[0:4]
     numeros = int(patente[4::])
-    ID = 0
+    id_patente = 0
 
     for i, letra in enumerate(reversed(letras)):
-        ID += letras_a_num[letra]*(10**(i+3))
-    ID += numeros + 1
+        id_patente += letras_a_num[letra]*(10**(i+3))
+    id_patente += numeros + 1
 
-    return ID
+    return id_patente
 
-# Pasa de un ID (en int) a una patente (en string)
-# Se asume una patente de la forma [A-Z]{4}[0-9]{3}
-def ID_A_Patente(id: int) -> str:
-    numero = int(str(id)[-3::]) - 1 if int(str(id)[-3::]) - 1 != -1 else 999
-    potencias = id - numero
+
+def id_a_patente(id_patente: int) -> str:
+    """ Pasa de un ID (en int) a una patente (en string). 
+        Se asume una patente de la forma [A-Z]{4}[0-9]{3} """
+    numero = int(str(id_patente)[-3::]) - \
+        1 if int(str(id_patente)[-3::]) - 1 != -1 else 999
+    potencias = id_patente - numero
     potencias //= 1000
     indices = str(potencias)
     resultado = ""
     for i in indices:
-        resultado += num_a_letras[int(i)] 
-    resultado = resultado.rjust(4, "A") # Rellenamos con A's si es que el num era chico
-    resultado += str(numero).rjust(3, "0") # appendeamos el numero de la patente (rellenando con 0s)
+        resultado += num_a_letras[int(i)]
+    # Rellenamos con A's si es que el num era chico
+    resultado = resultado.rjust(4, "A")
+    # appendeamos el numero de la patente (rellenando con 0s)
+    resultado += str(numero).rjust(3, "0")
     return resultado
 
+
 # Pruebas simples
-assert(Patente_A_ID("AAAA000") == 1)
-assert(Patente_A_ID("AAAB000") == 1001)
-assert(Patente_A_ID("AABA023") == 10024)
-assert(Patente_A_ID("AAAA999") == 1000)
-ID_A_Patente(1001)
-assert(ID_A_Patente(1) == "AAAA000")
-assert(ID_A_Patente(1001) == "AAAB000")
-assert(ID_A_Patente(10024) == "AABA023")
-assert(ID_A_Patente(1000) == "AAAA999")
+assert patente_a_id("AAAA000") == 1
+assert patente_a_id("AAAB000") == 1001
+assert patente_a_id("AABA023") == 10024
+assert patente_a_id("AAAA999") == 1000
+
+assert id_a_patente(1) == "AAAA000"
+assert id_a_patente(1001) == "AAAB000"
+assert id_a_patente(10024) == "AABA023"
+assert id_a_patente(1000) == "AAAA999"
